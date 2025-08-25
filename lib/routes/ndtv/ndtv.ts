22ba57a444ed64e3150deb241df739901fb9b6df
teczpert <86120734+teczpert@users.noreply.github.com>
2025-08-25 13:04:32 +0530

    Delete lib/routes/ndtv directory

--- lib/routes/ndtv/ndtv.ts
+++
@@ -1,7 +0,0 @@
-import { Namespace } from '@/types';
-
-export const namespace: Namespace = {
-    name: 'NDTV',
-    url: 'ndtv.com',
-    description: 'NDTV News',
-};
--- lib/routes/ndtv/router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from '@hono/hono';
-import { route as worldRoute } from './world';
-
-const router = new Router();
-
-router.get('/world', worldRoute.handler);
-
-export default router;
